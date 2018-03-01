493bfb54b7b859410e6a38c86033e15fbc9f2515
Rajeevkumar Yadav <rajeev11430@gmail.com>
2018-03-02 00:33:35 +0530

    Update index.php

--- paypal/index.php
+++ paypal/index.php
@@ -1,11 +1,9 @@
 <form name="paypalForm" action="paypal.php" method="post">
 <input type="hidden" name="id" value="123">
-<input type="hidden" name="CatDescription" value="Donation to bla bla">
-                       <input type="hidden" name="payment" value="10">  
+<input type="hidden" name="CatDescription" value="Donation to hb@TechRaY.com">
+                       <input type="hidden" name="payment" value="50">  
                        <input type="hidden" name="key" value="<? echo md5(date("Y-m-d:").rand()); ?>
-					   
-					   
-                           
+			
                                     
-<input TYPE="image" SRC="http://www.coachsbr.com/images/site/paypal_button.gif" name="paypal"  value="Payment via Paypal" >
+<input TYPE="image" SRC="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkMSLG7vuXd4ucHW9bbXi9MIH9Eg9Wkdq3njjjarZ5NfpJ3xaMZA" name="paypal"  value="Payment via Paypal" >
 </form>
