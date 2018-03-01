806a9b66b3d4fb2f9a3b4ba35a6c6e9d2aad288e
Rajeevkumar Yadav <rajeev11430@gmail.com>
2018-03-02 00:35:02 +0530

    done

--- paypal/index.php
+++ paypal/index.php
@@ -2,8 +2,7 @@
 <input type="hidden" name="id" value="123">
 <input type="hidden" name="CatDescription" value="Donation to hb@TechRaY.com">
                        <input type="hidden" name="payment" value="50">  
-                       <input type="hidden" name="key" value="<? echo md5(date("Y-m-d:").rand()); ?>
-			
+                       <input type="hidden" name="key" value="<? echo md5(date("Y-m-d:").rand()); ?>		
                                     
 <input TYPE="image" SRC="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkMSLG7vuXd4ucHW9bbXi9MIH9Eg9Wkdq3njjjarZ5NfpJ3xaMZA" name="paypal"  value="Payment via Paypal" >
 </form>
