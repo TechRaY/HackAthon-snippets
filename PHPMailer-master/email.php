c4ba59df25a265061a4b55a7c7d39fe196ce0520
Rajeevkumar Yadav <rajeev11430@gmail.com>
2018-06-07 18:19:24 +0530

    updated

--- PHPMailer-master/email.php
+++ PHPMailer-master/email.php
@@ -1,4 +1,8 @@
+
 <?php
+ 
+error_reporting(0);
+ 
 require_once('PHPMailerAutoload.php');
 $mail = new PHPMailer();
 //$mail->CharSet =  "utf-8";
@@ -6,17 +10,17 @@ $mail->IsSMTP();
 $mail->SMTPDebug= "1";  
 // enable SMTP authentication
 $mail->SMTPAuth = true;    
-$mail->SMTPSecure = "ssl";       
+$mail->SMTPSecure = "tls";       
 $mail->Host = "smtp.gmail.com";     
-$mail->Port = "465";    
+$mail->Port = "587";    
 $mail->IsHTML(true);
 // GMAIL username
-$mail->Username = "rsny14@gmail.com";
+$mail->Username = "rajeev11430@gmail.com";
 // GMAIL password
 $mail->Password = "";       // add the password
 
 // sets GMAIL as the SMTP server
-$mail->setFrom("rsny14@gmail.com");
+$mail->setFrom("rajeev11430@gmail.com");
 
 // set the SMTP port for the GMAIL server
 
@@ -25,12 +29,12 @@ $mail->setFrom("rsny14@gmail.com");
   // recepient address          this is functions one parameter
 $mail->Subject  =  'SMTP Mail Testing';
 
-$mail->Body    = 'Hi there , This is just a testing mail from php';    // you can give subject also as parameter
-$mail->AddAddress('rajeev11430@gmail.com'); 
+$mail->Body    = 'its working';    // you can give subject also as parameter
+$mail->AddAddress('rajeevkumar.yadav@ves.ac.in'); 
 if($mail->Send())
 {
 	echo "Message was  Send :)";             // specific messages to be sent as a parameter
-	//header('Location:index.php');
+	header('Location:index.php');
 }
 else
 {
